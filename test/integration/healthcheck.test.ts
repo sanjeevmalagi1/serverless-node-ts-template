6c60489def835178e385dff8e49da0f81bb8adfd
Sanjeev Malagi <sanjeevmalagi@gmail.com>
2025-06-22 07:57:54 +0530

    add tests with jest supertest

---
+++ test/integration/healthcheck.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+
+import app from 'app';
+
+describe('healthcheck route', () => {
+  describe('GET /healthcheck', () => {
+    test('should respond with 200 OK', async () => {
+      await request(app).get('/healthcheck').expect(200);
+    });
+  });
+});
