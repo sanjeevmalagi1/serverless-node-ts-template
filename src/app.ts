5fb9d387a242edffdd3f7764570e67e9513cf307
Sanjeev Malagi <sanjeevmalagi@gmail.com>
2025-06-22 18:11:39 +0530

    add prometheus and grafana

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
+import promClient from "prom-client"
 
 const app = express()
+const register = new promClient.Registry();
+
+promClient.collectDefaultMetrics({ register });
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/metrics', async (_req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.get('/healthcheck', async (_req: Request, res: Response) => {
   res.status(200).json({ status: "OK" })
